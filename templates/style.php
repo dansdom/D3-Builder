7c91f87711732fbff87e5ffab5769fbf091d4b7e
Daniel Thomson <dansdom@gmail.com>
2013-02-03 21:43:04 +1100

    fixed var for style file

--- templates/style.php
+++ templates/style.php
@@ -1,6 +1,6 @@
 <?php
 
-$style =  'html,body,div,span,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,address,cite,code,del,dfn,em,img,ins,kbd,q,samp,small,strong,sub,sup,var,b,i,dl,dt,dd,ol,ul,li,fieldset,form,hr,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,figcaption,figure,footer,header,hgroup,menu,nav,section,summary,time,mark,audio,video {margin:0;padding:0;border:0;font-size:100%;font:inherit;outline:0;vertical-align:baseline;-webkit-font-smoothing:antialiased}'."\n"
+$styleFile =  'html,body,div,span,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,address,cite,code,del,dfn,em,img,ins,kbd,q,samp,small,strong,sub,sup,var,b,i,dl,dt,dd,ol,ul,li,fieldset,form,hr,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,figcaption,figure,footer,header,hgroup,menu,nav,section,summary,time,mark,audio,video {margin:0;padding:0;border:0;font-size:100%;font:inherit;outline:0;vertical-align:baseline;-webkit-font-smoothing:antialiased}'."\n"
           .'article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section {display:block}'."\n"
           .'html {background:white;font-size:100%;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}'."\n"
           .'body {min-height:100%;font:13px/18px Helvetica,Arial,sans-serif}'."\n"
