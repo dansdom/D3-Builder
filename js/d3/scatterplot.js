30c64a8fc10f755f159c7f83919f7d23b7420171
daniel thomson <daniel.thomson@abc.net.au>
2014-12-17 11:27:03 +1100

    Realised I didn't need the checkMissingValues function on the scatterplot, so removed it

--- js/d3/scatterplot.js
+++ js/d3/scatterplot.js
@@ -494,10 +494,10 @@ var extend = extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                     "fill" : container.color(i),
                     "stroke" : container.color(i),
                     "stroke-opacity" : function(d) {
-                        if (d.y > 0) { return elements.opacity; } else { return 0; }
+                        if (d[dataStructure.y] > 0) { return elements.opacity; } else { return 0; }
                     },
                     "fill-opacity" : function(d) {
-                        if (d.y > 0) { return elements.opacity; } else { return 0; }
+                        if (d[dataStructure.y] > 0) { return elements.opacity; } else { return 0; }
                     }
                 });
 
@@ -525,10 +525,10 @@ var extend = extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                 })
                 .style({
                     "stroke-opacity" : function(d) {
-                        if (d.y > 0) { return elements.opacity; } else { return 0; }
+                        if (d[dataStructure.y] > 0) { return elements.opacity; } else { return 0; }
                     },
                     "fill-opacity" : function(d) {
-                        if (d.y > 0) { return elements.opacity; } else { return 0; }
+                        if (d[dataStructure.y] > 0) { return elements.opacity; } else { return 0; }
                     }
                 });
 
@@ -682,90 +682,8 @@ var extend = extend || function(){var h,g,b,e,i,c=arguments[0]||{},f=1,k=argumen
                 }
             }
             
-            // find any missing data and add a zero value for it
-            container.dataNest = container.checkMissingValues(nestedData);
-            container.dataLayers = container.stack(container.dataNest);
+            container.dataLayers = nestedData;
             //console.log(container.dataLayers);
-
-            return data;
-        },
-        // check for any missing values in the data set
-        checkMissingValues : function(data) {
-            var container = this,
-                layer,
-                layerValue,
-                layerX,
-                i, j,
-                duplicates = {},
-                xRange = [];
-
-            function checkGroups(range) {
-                var layer,
-                    i, j, k,
-                    hasPointX,
-                    emptyDataPoint,
-                    rangeValue,
-                    layerLength,
-                    rangeLength = range.length,
-                    dataLength = data.length;
-
-                // check each data point against the range and add a value if it doesn't exist
-                for (i = 0; i < rangeLength; i++) {
-                    rangeValue = range[i];
-                    // check over each data layer
-                    for (j = 0; j < dataLength; j++) {
-                        // check each item in the data group
-                        layer = data[j];
-                        layerLength = layer.values.length;
-                        hasPointX = false;
-                        for (k = 0; k < layerLength; k++) {
-                            // check the items value against the range value
-                            if (layer.values[k][container.opts.dataStructure.x].valueOf() === rangeValue.valueOf()) {
-                                hasPointX = true;
-                            }
-                        }
-                        // if the point is empty then assign a zero value
-                        if (!hasPointX) {
-                            //console.log('there is a missing value');
-                            emptyDataPoint = {};
-                            emptyDataPoint[container.opts.dataStructure.x] = rangeValue;
-                            emptyDataPoint[container.opts.dataStructure.y] = 0;
-                            emptyDataPoint[container.opts.dataStructure.key] = layer.key;
-                            layer.values.push(emptyDataPoint);
-                        }
-                    }
-                }
-            }
-
-            // construct the x range that the data will be mapped to
-            for (i = 0; i < data.length; i++) {
-                layer = data[i];
-                for (j = 0; j < layer.values.length; j++) {
-                    layerValue = layer.values[j];
-                    xRange.push(layerValue[container.opts.dataStructure.x]);
-                }
-            }
-
-            // store an object of array values
-            xRange = xRange.filter(function(item, index, inputArray) {
-                var hash = item.valueOf(),
-                    isDup = duplicates[hash];
-
-                duplicates[hash] = true;
-                return !isDup;
-            });
-
-            // fill in any missing values ifrom the x range
-            checkGroups(xRange);
-
-            function sortArrayValues(index) {
-                data[index].values.sort(function(a, b) { return d3.ascending(a[container.opts.dataStructure.x], b[container.opts.dataStructure.x]); });
-            }
-
-            // re-order each data layer
-            for (i = 0; i < data.length; i++) {         
-                sortArrayValues(i);
-            }
 
             return data;
         },
